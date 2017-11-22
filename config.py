a144c5e31b60a4bdde09e9d1cacc95f267af21e8
noi narisak <noi.narisak@gmail.com>
2017-11-21 23:39:04 -0600

    Add okta-oidc-python-flask files

---
+++ config.py
@@ -0,0 +1,9 @@
+okta = {
+    "org_host": "<okta org url>",
+    "api_token": "<okta api token>",
+    "oidc_client_id": "<okta oidc app client id>",
+    "oidc_client_secret": "<okta oidc app client secret>",
+    "redirect_uri": "<application oidc post handler url>",
+    "app_host": "<application url>",
+    "post_oidc_redirect": "<application url to redirect to after oidc token is granted>"
+}
